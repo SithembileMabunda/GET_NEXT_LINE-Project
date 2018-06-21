2f3c0918685e5677ceff6f5f39215bc2f137270a
Sithembile <39857574+SithembileMabunda@users.noreply.github.com>
2018-06-21 12:20:48 +0200

    GET_NEXT_LINE

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smabunda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/19 15:29:53 by smabunda          #+#    #+#             */
+/*   Updated: 2018/06/19 15:35:26 by smabunda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <<sys/types.h>
+# include <sys/uio.h>
+# include "libft.h"
+
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
---
+++ main.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smabunda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/19 15:44:03 by smabunda          #+#    #+#             */
+/*   Updated: 2018/06/20 15:09:27 by smabunda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_nex_line.h"
+#include "libft.h"
+#include <fcntl.h>
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		free(line);
+	}
+	if (argc == 2)
+		close (fd);
+}
