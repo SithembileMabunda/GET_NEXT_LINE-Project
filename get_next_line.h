b8a096094977a30c23b00c4300ab894c33eac301
Sithembile <39857574+SithembileMabunda@users.noreply.github.com>
2018-07-03 08:54:48 +0200

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,17 @@
 /*   By: smabunda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 15:29:53 by smabunda          #+#    #+#             */
-/*   Updated: 2018/06/19 15:35:26 by smabunda         ###   ########.fr       */
+/*   Updated: 2018/07/02 14:42:21 by smabunda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <<sys/types.h>
+# include <sys/types.h>
 # include <sys/uio.h>
-# include "libft.h"
+# include "libft/libft.h"
+# include <fcntl.h>
 
 # define BUFF_SIZE 32
 
